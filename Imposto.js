918885fc724029404ce5b7ebd2af97339a899ee9
José Gomes <josegomes09434@gmail.com>
2023-10-31 19:43:54 -0300

    commit

---
+++ Imposto.js
@@ -0,0 +1,9 @@
+let salario= 1200.00
+function imposto(){
+ if(salario <= 2000.00){
+    console.log('isento')
+ }else{
+    console.log('sujeito a impostos')
+ }
+}
+imposto()
---
+++ Multiplos.js
@@ -0,0 +1,10 @@
+let a= 10
+let b= 5
+function mult(){
+if( a%b === 0 || b%a === 0 ){
+    console.log('multiplos')
+}else{
+    console.log('não multiplos')
+}
+}
+mult()
